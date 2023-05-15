3e469898d1a7291173b126e43e26eb45a90066de
WahidDh <WahidDh@MSI>
2023-05-15 13:48:38 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Trupja_MyTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
